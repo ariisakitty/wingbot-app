f0ff88371eec33d9e2572c772b03c6363980ff70
Chris Lee <ariisakitty@gmail.com>
2023-07-07 04:15:29 +0000

    A lot of updates

--- app.py
+++ app.py
@@ -2,78 +2,107 @@ from flask import Flask, render_template, request
 from datetime import datetime
 import threading
 import time
+import tools, msg_processing
 
 app = Flask(__name__)
 
+r2s_msg = {
+    "loc": {
+        "lat": None,
+        "lng": None
+    },
+    "gate": None,
+    "e-stop": False,
+    "fod": [],
+    "left": [],
+    "right": [],
+    "is_activated": False,
+    "is_engaged": False
+}
+
+s2r_msg = {
+    "gate": None,
+    "activate": False,
+    "engage": False
+}
+
 robot_readiness = {
-    "status": "Ready",
+    "status": None,
     "location": {
-        "lat": 39.049363,
-        "lng": -84.654340
-    }
+        "lat": None,
+        "lng": None
+    },
+    "e_stop": False
 }
-def toggle_robot_readiness():
-    global robot_readiness 
-
-    if robot_readiness["status"] == "Ready":
-        robot_readiness["status"] = "Unavailable"
-        robot_readiness["location"]["lat"] = 39.04960844154871
-        robot_readiness["location"]["lng"] = -84.6545837816287        
-    else:
-        robot_readiness["status"] = "Ready"
-        robot_readiness["location"]["lat"] = 39.049363
-        robot_readiness["location"]["lng"] = -84.654340
-    print(robot_readiness)
-    threading.Timer(5, toggle_robot_readiness).start()
-toggle_robot_readiness()
-
 gate_readiness = {
-    "gate": "A14",
+    "gate": None,
     "FOD": False,
-    "FOD_location": [{
-        "lat": None,
-        "lng": None
-        # "lat": 39.049363, 
-        # "lng": -84.654340 
-    }]
+    "FOD_location": []
 }
-def toggle_gate_readiness ():
-    global gate_readiness
-    if gate_readiness["gate"] == "A14":
-        gate_readiness["gate"] = "A16"
-    else:
-        gate_readiness["gate"] = "A14"
+wingwalking = {
+    "is_activated": False,
+    "is_engaged": False,
+    "left": None,
+    "left_location": [],
+    "right": None,
+    "right_location": []
+}
 
-    if gate_readiness["FOD"]:
-        gate_readiness["FOD"] = False
-        gate_readiness["FOD_location"][0]["lat"] = None
-        gate_readiness["FOD_location"][0]["lng"] = None
-    else:
-        gate_readiness["FOD"] = True
-        gate_readiness["FOD_location"][0]["lat"] = 39.04964018403017
-        gate_readiness["FOD_location"][0]["lng"] = -84.65421236479904
-    print(gate_readiness)
-    threading.Timer(8, toggle_gate_readiness).start()
-toggle_gate_readiness()
+counter = 0
+def toggle_r2s():
+    global r2s_msg
+    global robot_readiness, gate_readiness, wingwalking
+    global counter
 
-ww_left = {
-    "object": False,
-    "object_location": {
-        "lat": None,
-        "lng": None
-    }
-}
-ww_right = {
-    "object": False,
-    "object_location": {
-        "lat": None,
-        "lng": None
-    }
-}
+    if counter == 0:
+        counter = 1
+    elif counter == 1:
+        r2s_msg = tools.r2s_msg_1
+        counter = 2
+    elif counter == 2:
+        r2s_msg = tools.r2s_msg_2
+        counter = 3
+    elif counter == 3:
+        r2s_msg = tools.r2s_msg_3
+        counter = 4
+    elif counter == 4:
+        r2s_msg = tools.r2s_msg_4
+        counter = 5
+    elif counter == 5:
+        r2s_msg = tools.r2s_msg_5
+        counter = 6
+    elif counter == 6:
+        r2s_msg = tools.r2s_msg_6
+        counter = 7
+    elif counter == 7:
+        r2s_msg = tools.r2s_msg_7
+        counter = 8
+    elif counter == 8:
+        r2s_msg = tools.r2s_msg_8
+        counter = 9
+    elif counter == 9:
+        r2s_msg = tools.r2s_msg_9
+        counter = 10
+    elif counter == 10:
+        r2s_msg = tools.r2s_msg_10
+        counter = 11
+    elif counter == 11:
+        r2s_msg = tools.r2s_msg_11
+        counter = 0
+
+    print(f"[{counter}] / r2s_msg: {r2s_msg}")
+    
+    robot_readiness, gate_readiness, wingwalking = msg_processing.input_proc(r2s_msg)
+    print(f"robot_readiness: {robot_readiness}")
+    print(f"gate_readiness: {gate_readiness}")
+    print(f"wingwalking: {wingwalking}")
+    
+    threading.Timer(5, toggle_r2s).start()
+toggle_r2s()
 
 @app.route('/')
 def index():
-    return render_template('index.html', robot_readiness=robot_readiness, gate_readiness=gate_readiness, ww_left=ww_left, ww_right=ww_right)
+    return render_template('index.html', robot_readiness=robot_readiness, gate_readiness=gate_readiness, wingwalking=wingwalking)
 
 @app.route('/get_robot_readiness')
 def get_robot_readiness():
@@ -82,6 +111,10 @@ def get_robot_readiness():
 @app.route('/get_gate_readiness')
 def get_gate_readiness():
     return gate_readiness
+
+@app.route('/get_wingwalking_status')
+def get_wingwalking_readiness():
+    return wingwalking
 
 # @app.route('/', methods=['POST'])
 # def handle_message():
@@ -97,3 +130,4 @@ def get_gate_readiness():
 if __name__ == '__main__':
     app.run(port=5001)
     # app.run(host='0.0.0.0', port=5000)
+
---
+++ tools.py
@@ -0,0 +1,170 @@
+r2s_msg_1 = {
+    "loc": {
+        "lat": 39.049363,
+        "lng": -84.654340
+    },
+    "gate": None,
+    "e-stop": False,
+    "fod": [],
+    "left": [],
+    "right": [],
+    "is_activated": False,
+    "is_engaged": False
+}
+r2s_msg_2 = {
+    "loc": {
+        "lat": 39.04960844154871,
+        "lng": -84.6545837816287
+    },
+    "gate": "A14",
+    "e-stop": False,
+    "fod": [],
+    "left": [],
+    "right": [],
+    "is_activated": False,
+    "is_engaged": False
+}
+r2s_msg_3 = {
+    "loc": {
+        "lat": 39.049363,
+        "lng": -84.654340
+    },
+    "gate": "A14",
+    "e-stop": False,
+    "fod": [
+        {
+            "lat": 39.04964018403017,
+            "lng": -84.65421236479904
+        },
+        {
+            "lat": 39.04950317919271,
+            "lng": -84.65405445409232
+        }],
+    "left": [],
+    "right": [],
+    "is_activated": False,
+    "is_engaged": False
+}
+r2s_msg_4 = {
+    "loc": {
+        "lat": 39.04960844154871,
+        "lng": -84.6545837816287
+    },
+    "gate": "A14",
+    "e-stop": False,
+    "fod": [],
+    "left": [],
+    "right": [],
+    "is_activated": False,
+    "is_engaged": False
+}
+r2s_msg_5 = {
+    "loc": {
+        "lat": 39.049363,
+        "lng": -84.654340
+    },
+    "gate": "A14",
+    "e-stop": False,
+    "fod": [],
+    "left": [],
+    "right": [],
+    "is_activated": True,
+    "is_engaged": False
+}
+r2s_msg_6 = {
+    "loc": {
+        "lat": 39.04960844154871,
+        "lng": -84.6545837816287
+    },
+    "gate": "A14",
+    "e-stop": False,
+    "fod": [],
+    "left": [
+        {
+            "lat": 39.04921202412433,
+            "lng": -84.65387185629405
+        },
+        {
+            "lat": 39.04921805843844,
+            "lng": -84.6535513388822
+        }
+    ],
+    "right": [
+        {
+            "lat": 39.04921504128145,
+            "lng": -84.6546993739756
+        }
+    ],
+    "is_activated": True,
+    "is_engaged": False
+}
+r2s_msg_7 = {
+    "loc": {
+        "lat": 39.049363,
+        "lng": -84.654340
+    },
+    "gate": "A14",
+    "e-stop": False,
+    "fod": [],
+    "left": [],
+    "right": [
+        {
+            "lat": 39.04921504128145,
+            "lng": -84.6546993739756
+        }
+    ],
+    "is_activated": True,
+    "is_engaged": False
+}
+r2s_msg_8 = {
+    "loc": {
+        "lat": 39.04960844154871,
+        "lng": -84.6545837816287
+    },
+    "gate": "A14",
+    "e-stop": False,
+    "fod": [],
+    "left": [],
+    "right": [],
+    "is_activated": False,
+    "is_engaged": False
+}
+r2s_msg_9 = {
+    "loc": {
+        "lat": 39.049363,
+        "lng": -84.654340
+    },
+    "gate": "A14",
+    "e-stop": True,
+    "fod": [],
+    "left": [],
+    "right": [],
+    "is_activated": False,
+    "is_engaged": False
+}
+r2s_msg_10 = {
+    "loc": {
+        "lat": 39.049363,
+        "lng": -84.654340
+    },
+    "gate": "A14",
+    "e-stop": False,
+    "fod": [],
+    "left": [],
+    "right": [],
+    "is_activated": False,
+    "is_engaged": False
+}
+r2s_msg_11 = {
+    "loc": {
+        "lat": None,
+        "lng": None
+    },
+    "gate": None,
+    "e-stop": False,
+    "fod": [],
+    "left": [],
+    "right": [],
+    "is_activated": False,
+    "is_engaged": False
+}
